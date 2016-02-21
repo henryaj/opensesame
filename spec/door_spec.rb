1308c0f759f74a3b4cf5f600bb720fd1a783071b
Henry Stanley <henry@henrystanley.com>
2016-02-21 22:27:08 +0000

    Fix up test to use double rather than real object

--- spec/door_spec.rb
+++ spec/door_spec.rb
@@ -1,9 +1,12 @@
 require "door"
 
 describe OpenSesame::Door do
+  let(:particle) { class_double(Particle, device: device) }
+  let(:device) { instance_double(Particle::Device,
+    call: nil, name: "my-little-particle")
+  }
+
   describe "#open" do
-    let(:particle) { class_double(Particle, device: device) }
-    let(:device) { instance_double(Particle::Device, call: nil) }
     let(:door) { OpenSesame::Door.new(device: device)}
 
     it "instructs the Particle to open the door" do
@@ -16,6 +19,8 @@ describe OpenSesame::Door do
   it "takes the device name from PARTICLE_DEVICE_NAME by default" do
     expect(ENV).to receive(:fetch).with("PARTICLE_DEVICE_NAME").
       and_return("my-little-particle")
+    expect(Particle).to receive(:device).with("my-little-particle").
+      and_return(device)
 
     door = OpenSesame::Door.new
     expect(door.device.name).to eq "my-little-particle"
